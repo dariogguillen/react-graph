3875ccaa78f179aafad792c10b2fffc14c2532db
dariogguillen <guillendario@gmail.com>
2018-11-06 17:36:27 -0600

    adding views

--- client/components/LyricCreate.js
+++ client/components/LyricCreate.js
@@ -1,14 +1,53 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import gql from 'graphql-tag'
+import { graphql } from 'react-apollo'
+
+const mutation = gql`
+  mutation AddLyricToSong($content: String, $songId: ID) {
+    addLyricToSong(content: $content, songId: $songId) {
+      id
+      title
+      lyrics {
+        content
+      }
+    }
+  }
+`
 
 class LyricCreate extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { content: '' }
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+
+  onSubmit(e) {
+    e.preventDefault()
+    const { mutate, songId } = this.props
+    const { content } = this.state
+
+    mutate({
+      variables: {
+        songId,
+        content
+      }
+    })
+    this.setState({ content: '' })
+  }
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.onSubmit}>
         <label>Add a Lyric</label>
-        <input type="text"/>
+        <input
+          value={this.state.content}
+          onChange={e => this.setState({ content: e.target.value })}
+          type="text"
+        />
       </form>
-    );
+    )
   }
 }
 
-export default LyricCreate;
+export default graphql(mutation)(LyricCreate)
--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -3,24 +3,27 @@ import { graphql } from 'react-apollo'
 import { Link } from 'react-router'
 
 import fetchSong from '../queries/fetchSong'
-import LyricCreate from './LyricCreate';
+import LyricCreate from './LyricCreate'
+import LyricList from './LyricList'
 
 class SongDetail extends Component {
   render() {
     const { song } = this.props.data
-    if (!song) return <div />
+    console.log(song)
+    if (!song) return <div>Loading...</div>
     return (
       <div>
         <Link to="/">Back</Link>
         <h3>{song.title}</h3>
-        <LyricCreate />
+        <LyricList />
+        <LyricCreate songId={song.id} />
       </div>
     )
   }
 }
 
 export default graphql(fetchSong, {
-  options: ({ params }) => {
-    return { variables: { id: params.id } }
+  options: props => {
+    return { variables: { id: props.params.id } }
   }
 })(SongDetail)
